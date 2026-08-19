093340e574ac24be44eaa7b38c1194c8316bb1aa
lse872317312010 <57588360+lse872317312010@users.noreply.github.com>
2026-08-20 01:45:02 +0900

    feat(wave1): add policy application ports and in-memory adapter

---
+++ packages/application/lib/application.dart
@@ -0,0 +1,7 @@
+library personal_os_application;
+
+export 'src/appearance_commands.dart';
+export 'src/appearance_queries.dart';
+export 'src/appearance_use_case.dart';
+export 'src/application_ports.dart';
+
