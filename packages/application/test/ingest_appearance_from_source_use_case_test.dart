c4e621e49a7c2d2e53ff3915a34ecdc683359791
lse872317312010 <57588360+lse872317312010@users.noreply.github.com>
2026-08-24 17:11:07 +0800

    style: remove application test trailing whitespace

--- packages/application/test/ingest_appearance_from_source_use_case_test.dart
+++ packages/application/test/ingest_appearance_from_source_use_case_test.dart
@@ -101,7 +101,7 @@ void main() {
     expect(ingestion.discarded, [BlobRef('blob://opaque-source-1')]);
     expect(store.events, hasLength(1));
   });
-  
+
   IngestAppearanceFromSourceUseCase _useCase(
     _SourceIngestion ingestion,
     _Store store,
