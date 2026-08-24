d7cbd0f2eaf127f9e50754d2ad9660ac44acf85d
lse872317312010 <57588360+lse872317312010@users.noreply.github.com>
2026-08-24 19:10:47 +0800

    build(android): align root Gradle script with Flutter 3.47

--- apps/personal_os_app/android/build.gradle.kts
+++ apps/personal_os_app/android/build.gradle.kts
@@ -5,11 +5,18 @@ allprojects {
     }
 }
 
-val sharedBuildDirectory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(sharedBuildDirectory)
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    project.layout.buildDirectory.value(sharedBuildDirectory.dir(project.name))
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
