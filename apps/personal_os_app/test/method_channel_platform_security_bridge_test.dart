3aab286ff5209b52dad42936724eaebd59b966bc
lse872317312010 <57588360+lse872317312010@users.noreply.github.com>
2026-09-14 23:30:52 +0800

    fix(test): remove redundant typed data import

--- apps/personal_os_app/test/method_channel_platform_security_bridge_test.dart
+++ apps/personal_os_app/test/method_channel_platform_security_bridge_test.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:personal_os_app/src/composition/android_platform_security_bridge.dart';
